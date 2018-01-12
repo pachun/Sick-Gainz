8fd813eedf1bafb62fa7310a751db7460e3e5995
Nicholas Pachulski <pachun@Nicholass-MBP.fios-router.home>
2018-01-12 16:51:59 -0500

    Show editable cost value in app (not today widget)

--- App.js
+++ App.js
@@ -1,62 +1,86 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React from 'react'
 import {
   AppRegistry,
   Platform,
-  StyleSheet,
   Text,
-  View
-} from 'react-native';
+  TextInput,
+  View,
+} from 'react-native'
+import { FormattedCurrency, FormattedWrapper } from 'react-native-globalize'
 import { DevMenu } from 'react-native-today-widget'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+const style = {
+  stackChildrenVertically: {
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:'center',
+    paddingTop:100,
+    width:'100%',
+  },
+  heightConstraint: {
+    width:'100%',
+    height:100,
+  },
+  showFormattedCurrencyUnderInputTextField: {
+    width:'100%',
+    position: 'absolute',
+    height:'100%',
+  },
+  cost: {
+    height: '100%',
+    lineHeight: 100,
+    fontSize:32,
+    fontWeight:'bold',
+    textAlign: 'center',
+  },
+  costField: {
+    width:'100%',
+    height:'100%',
+    backgroundColor:'transparent',
+    color:'transparent',
+    fontSize:56,
+    textAlign:'center',
+  },
+  margins: {
+    flexDirection:'column',
+    width:'80%',
+  },
+  costLabel: {
+    fontSize:16,
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+class App extends React.Component<{}> {
+  state = { cost: '' }
+
+  cost = () => parseFloat( this.state.cost.length > 0 ? this.state.cost : 0 )
+
+  render = () =>
+    <FormattedWrapper locale="en" currency="USD">
+      <View style={style.stackChildrenVertically}>
+        <View style={style.margins}>
+
+          <Text style={style.costLabel}>Cost</Text>
+          <View style={style.heightConstraint}>
+            <View style={style.showFormattedCurrencyUnderInputTextField}>
+              <FormattedCurrency value={this.cost()} currency="USD" style={style.cost}/>
+            </View>
+            <TextInput style={style.costField}
+              testID="Cost Value"
+              selectionColor="transparent"
+              clearTextOnFocus={true}
+              keyboardType="decimal-pad"
+              returnKeyType="done"
+              onChangeText={(cost) => this.setState({ cost }) }
+            />
+          </View>
+
+        </View>
+      </View>
+    </FormattedWrapper>
+}
+
+export default App
 
 const TodayWidget = () =>
   <View>
---
+++ e2e/features/editCost.js
@@ -0,0 +1,43 @@
+describe('Sick Gainz Home Screen', () => {
+
+  const cost = () => element(by.id('Cost Value'))
+  const doneWithKeyboardButton = () => element(by.label('Done').and(by.type('UIButtonLabel')))
+
+  it('displays a $0.00 cost value as an empty state', async () => {
+    await expect(element(by.label('$0.00'))).toExist()
+  })
+
+  it('formats cost values', async () => {
+    await cost().tap()
+    await cost().typeText("1")
+    await doneWithKeyboardButton().tap()
+
+    await expect(element(by.label('$1.00'))).toBeVisible()
+
+    await cost().tap()
+    await cost().typeText("1.1")
+    await doneWithKeyboardButton().tap()
+
+    await expect(element(by.label('$1.10'))).toBeVisible()
+
+    await cost().tap()
+    await cost().typeText("1337.66")
+    await doneWithKeyboardButton().tap()
+
+    await expect(element(by.label('$1,337.66'))).toBeVisible()
+  })
+
+  it('rounds cost values', async () => {
+    await cost().tap()
+    await cost().typeText("0.555")
+    await doneWithKeyboardButton().tap()
+
+    await expect(element(by.label('$0.56'))).toBeVisible()
+
+    await cost().tap()
+    await cost().typeText("0.554")
+    await doneWithKeyboardButton().tap()
+
+    await expect(element(by.label('$0.55'))).toBeVisible()
+  })
+})
